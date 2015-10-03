9268b43bc98c46c07f516a2cd823831f62f34746
Haoul <Haoul@Haoul-PC>
2015-10-03 12:06:15 +0900

    commit

--- haoul/src/study/Exercise4_3.scala
+++ haoul/src/study/Exercise4_3.scala
@@ -5,4 +5,15 @@ package study
  */
 object Exercise4_3 {
   
+  def main(args: Array[String]): Unit = {
+    getSum(10)
+  }
+  
+  def getSum(num: Int): Unit = {
+    println("1부터 " + num + "까지의 총 합은 " + setSum(num))
+  }
+  
+  def setSum(num: Int): Unit = {
+    
+  }
 }
