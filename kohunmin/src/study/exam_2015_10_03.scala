c64f17493a1b52fd9061cf95f327bdfb684974cf
ko hunmin <khmsoul@gmail.com>
2015-10-03 11:46:02 +0900

    출력까지잘

--- kohunmin/src/study/exam_2015_10_03.scala
+++ kohunmin/src/study/exam_2015_10_03.scala
@@ -1,17 +1,14 @@
-package playscala_kohunmin.study
+package study
 
 object exam_2015_10_03 {
     def main(args: Array[String]): Unit = {
-       //println (exam4_3(4))
+       println (exam4_3(4))
     }
-/*
-    def exam4_3(index: Int): Int = {
-        val temp
+    def exam4_3(index: Int) = {
+        var temp = 0
         for (i <- 1 to index)
-            for (j <- 1 to i)
-                temp += j
+          println(i)
     }    
-    */
 }
 
 
