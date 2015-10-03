533e41f96056f807446704b2c8f6b649b89acff1
ko hunmin <khmsoul@gmail.com>
2015-10-03 12:03:14 +0900

    고민

--- kohunmin/src/study/exam_2015_10_03.scala
+++ kohunmin/src/study/exam_2015_10_03.scala
@@ -2,13 +2,28 @@ package study
 
 object exam_2015_10_03 {
     def main(args: Array[String]): Unit = {
-       println (exam4_3(4))
+       println (exam4_3(10))
+       println (exam4_4(100))
     }
-    def exam4_3(index: Int) = {
+    def exam4_3(index: Int): Int = {
         var temp = 0
         for (i <- 1 to index)
-          println(i)
-    }    
+          for(j <- 1 to i)
+            temp += j
+        temp
+    } 
+    def exam4_4(max: Int): Int = {
+      var sumNum = 0
+      var index = 0
+      while(sumNum >= max) {
+        index += 1;
+        if(index % 2 == 0)
+          sumNum -= index
+        else
+          sumNum += index
+      }
+      return index
+    }
 }
 
 
