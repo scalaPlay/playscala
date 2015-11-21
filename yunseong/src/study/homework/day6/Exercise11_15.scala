40c2f84b7b33165f5cccdfa690d9cd7364580de5
oxide <sharlean@naver.com>
2015-11-21 22:19:08 +0900

    임시 커밋분.

---
+++ yunseong/src/study/homework/day6/Exercise11_15.scala
@@ -0,0 +1,29 @@
+package study.homework.day6
+
+//import com.github.nscala_time.time.Imports._
+
+/**
+ * @author Yunseong
+ */
+object Exercise11_15 {
+  def main(args: Array[String]): Unit = {
+    /*
+    val pattern: String = "yyyy-MM-dd"
+    
+    val a: DateTime = DateTime.parse("2015-11-25", DateTimeFormat.forPattern(pattern))
+    
+    print("a : " + a)
+    
+    println("b")
+    */
+  }
+  
+  def printResult(from: DateTime, to: DateTime): Unit = {
+    
+    payCheckCount(from, to)
+  }
+  
+  def payCheckCount(from: DateTime, to: DateTime): String = {
+    ""
+  }
+}
