ca149b06ae16bfdb60c4fa9692ee3085fd960460
ko hunmin <khmsoul@gmail.com>
2015-11-29 12:42:42 +0900

    11월 29일 커

--- kohunmin/src/study/inheritance/Element.scala
+++ kohunmin/src/study/inheritance/Element.scala
@@ -1,50 +1,70 @@
 package study.inheritance
 import Element.elem
 
-object Element{
+object Element {
   private class ArrayElement(
-      val contents: Array[String]
-  ) extends Element
-  
+    val contents: Array[String]) extends Element
+
   private class LineElement(s: String) extends Element {
     val contents = Array(s)
     override def width = s.length
     override def height = 1
   }
-  
+
   private class UniformElement(
-      ch: Char,
-      override val width: Int,
-      override val height: Int
-      ) extends Element {
+    ch: Char,
+    override val width: Int,
+    override val height: Int) extends Element {
     private val line = ch.toString * width
     def contents = Array.fill(height)(line)
   }
-  def elem(contens: Array[String]): Element = 
+  def elem(contens: Array[String]): Element =
     new ArrayElement(contens)
-  def elem(chr: Char, width: Int, height: Int): Element = 
+  def elem(chr: Char, width: Int, height: Int): Element =
     new UniformElement(chr, width, height)
-  def elem(line: String) : Element =
+  def elem(line: String): Element =
     new LineElement(line)
 }
 
 abstract class Element {
   def contents: Array[String]
-  
-  def width: Int = 
-    if(height == 0) 0 else contents(0).length()
-    
+
+  def width: Int = contents(0).length()
+
   def height: Int = contents.length
+
+  def above(that: Element): Element = {
+    val this1 = this widen that.width
+    val that1 = that widen this.width
+    elem(this1.contents ++ that1.contents)
+  }
   
-  def above(that: Element): Element = 
-   elem(this.contents ++ that.contents)
-  
-  def beside(that: Element): Element = 
+
+  def beside(that: Element): Element = {
+    val this1 = this heighten that.height
+    val that1 = that heighten this.height
     elem(
       for (
-          (line1, line2) <- this.contents zip that.contents
-          ) yield line1 + line2
-    )
+        (line1, line2) <- this1.contents zip that1.contents
+      ) yield line1 + line2)
+  }
+  
+  def widen(w: Int) : Element =
+    if (w <= width) this
+    else {
+      val left = elem(' ', (w - width) / 2, height)
+      val right = elem(' ', w - width - left.width, height)
+      left beside this beside right
+    }
+  
+  def heighten(h: Int) : Element = 
+    if ( h <= height) this
+    else {
+      val top = elem(' ', width, (h - height) / 2)
+      val bot = elem(' ', width, h - height - top.height)
+      top above this above bot
+    }
+
   override def toString = contents mkString "\n"
 }
 
@@ -61,11 +81,10 @@ class LineElement(s: String) extends Element {
   override def height = 1
 }
 
-class UniformElement (
-    ch: Char,
-    override val width: Int,
-    override val height: Int
-) extends Element {
+class UniformElement(
+  ch: Char,
+  override val width: Int,
+  override val height: Int) extends Element {
   private val line = ch.toString() * width
   def contents = Array.fill(height)(line)
 }
