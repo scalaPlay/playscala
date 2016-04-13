a38cae21f7e6f5cfa27dc37c999f576f1ba1729c
hunmin <hunmin@widerplanet.com>
2016-04-13 11:07:05 +0900

    1일차 틱택톡

--- kohunmin/src/sevenLanguage/oneDay/exam/tictac.scala
+++ kohunmin/src/sevenLanguage/oneDay/exam/tictac.scala
@@ -3,37 +3,37 @@
 /**
   * Created by kohunmin on 2016. 4. 2..
   */
-class mark
-class tic extends mark
-class tac extends mark
 
 class tictac {
 
-
   // Map과 같은 형태로 저장한다
-  var board = Array.tabulate[mark](10,10)
+  var board = Array.fill[String](3, 3)(".")
+
+
+  def setMark(x: Int, y: Int, mark: String): Unit = {
+    board(y)(x) = mark
+    println(board.map(_.mkString(" ")).mkString("\n"))
+  }
 
 
   // 좌표와 mark를 집어넣은다
-  def run(x : Int , y : Int , mark: mark): Unit ={
-    print(board)
-
-    return estimate()
-  }
-
-  def set(x : Int , y : Int , mark: mark): Unit = {
-
+  def run(x: Int, y: Int, mark: String): Unit = board(x)(y) match {
+    case "." => printf("(%d,%d) = %s \n잘 두었습니다\n",x,y,mark) ; setMark(x, y, mark)
+    case _   => printf("(%d,%d) 이미 그자리에 두었습니다\n",x,y)
   }
 
   /**
-    *  계산하여 이긴사람을 반환하거나 없으면 빈값을 반환한다
+    * 계산하여 이긴사람을 반환하거나 없으면 빈값을 반환한다
     */
   def estimate(): Unit = {
 
   }
+}
 
-  def main(args: Array[String]) {
+object main {
+  def main(args: Array[String]): Unit = {
     val tictac = new tictac()
-    tictac.run(1,2,new tic)
+    tictac.run(1, 2, "O")
+    tictac.run(2, 2, "O")
   }
 }
